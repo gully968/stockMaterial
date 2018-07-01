750549e274a1dd897ca7f01e8a15684480dacb83
marcelolav <marcelo.lavandeira@gmail.com>
2018-06-30 21:13:15 -0300

    Arreglos y listado de productos ordenes en otros listados

--- src/app/listados/listado-productos/listado-productos.component.ts
+++ src/app/listados/listado-productos/listado-productos.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { ProductosService } from '../../servicios/productos.service';
+import { Productos } from '../../clases/productos';
+import { MatTableDataSource, MatPaginator, MatSort, PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-listado-productos',
   templateUrl: './listado-productos.component.html',
   styleUrls: ['./listado-productos.component.css']
 })
-export class ListadoProductosComponent implements OnInit {
+export class ListadoProductosComponent implements AfterViewInit {
 
-  constructor() { }
+  displayedColumns = ['codigo', 'nombre', 'rubro', 'precioCompra', 'precioVenta', 'cantidadMinima', 'cantidadActual', 'observaciones'];
+  dataSource = new MatTableDataSource<Productos>();
+  length = 100;
+  pageSize = 5;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+  pageEvent: PageEvent;
 
-  ngOnInit() {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(public ps: ProductosService) { }
+
+  ngAfterViewInit(){
+    this.ps.getProductosObservable().subscribe(data => {
+      this.dataSource.data = data ;
+    })
+      this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); 
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
   }
 
 }
